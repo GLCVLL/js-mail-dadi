52e2e1e4d2a62e6b42333cfa44fb07c752f1d957
GLCVLL <g.ioele96@outlook.it>
2023-05-31 18:20:30 +0200

    added evenListeners

--- js/scriptDice.js
+++ js/scriptDice.js
@@ -5,6 +5,43 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto.
 Stampare (in console)*/
 
 
-const numbers
+//get element from DOM
+
+const buttonElement = document.getElementById('playbtn');
+
+// where the fun starts
+
+buttonElement.addEventListener("click", function(){
+
+    // get element from DOM
+
+    const userParagraph = document.getElementById('user-number');
+    const cpuParagraph = document.getElementById('cpu-number');
+    const resultParagraph = document.getElementById('results');
+
+    // make random numbers for user
+
+    const randomUserNumbers = parseInt(Math.floor(Math.random() * 6) + 1);
+
+    // make random numbers for cpu
+
+    const randomCpuNumbers = parseInt(Math.floor(Math.random() * 6) + 1);
+
+    // print numbers to the DOM
+
+    userParagraph.innerText = "Your number: " + randomUserNumbers;
+    cpuParagraph.innerText = "My number: " + randomCpuNumbers;
+
+    // make the winner of the game
+
+    if (randomCpuNumbers > randomUserNumbers) {
+        resultParagraph.innerText = "I win! try your luck again";
+      } else if (randomCpuNumbers < randomUserNumbers) {
+        resultParagraph.innerText = "Congratulation, you win!";
+      } else {
+        resultParagraph.innerText = "It's a tie!";
+      }
+
+});
 
  
